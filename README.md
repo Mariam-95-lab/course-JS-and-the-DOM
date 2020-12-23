910fad33da496c852668876165d39802d002283a
Mariam-95-lab <76123893+Mariam-95-lab@users.noreply.github.com>
2020-12-23 19:26:13 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Rubrics](#Rubrics)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Rubrics
+Your project will be evaluated by a Udacity code reviewer according to the Landing Page `project rubric`. Please make sure to re-review the rubric for detailed project requirements prior to submission.
---
+++ app.js
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+
+const sections = document.querySelectorAll('section');
+const navbarlist = document.getElementById('navbar__list');
+let numberOfListItems = sections.length;
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+function createListItem(id){
+	for (section of sections) {
+		sectionName= section.getAttribute('data-nav');
+		sectionlink = section.getAttribute('id');
+		ListItem = document.createElement('li');
+		ListItem.innerHTML = `<a class='menu__link' href='#${sectionlink}'>${sectionName}</a>` ;
+		navbarlist.appendChild(ListItem);
+	}
+}
+createListItem();
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+function sectionInViewPort(m){
+	let sectionPos = elem.getBoundingClientRect();
+	return (sectionPos.top >= 0);
+}
+function toggleActiveClass(){
+	for (section of sections){
+		if (sectionInViewPort(section)){
+			if (!section.classList.contains('your-active-class')) {
+			section.classList.add('your-active-class')
+
+		} else{
+			section.classList.remove('your-active-class')
+		}
+	}}
+}
+// build the nav
+
+document.addEventListener('scroll',toggleActiveClass)
+
